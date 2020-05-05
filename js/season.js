f99a99560aac3f2e99880f12ea070d823a46045a
ToeBeeBee <ToeBeeBee@users.noreply.github.com>
2020-05-06 00:17:18 +0100

    Initial Commit

---
+++ js/season.js
@@ -0,0 +1,321 @@
+import * as THREE from './build/three.module.js';
+import {SVGLoader} from './examples/jsm/loaders/SVGLoader.js';
+
+const vshader = `
+uniform float size;
+
+
+void main() {
+
+
+    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+
+    gl_PointSize = size * ( 300.0 / -mvPosition.z );
+
+    gl_Position = projectionMatrix * mvPosition;
+
+}
+`;
+const fshader = `
+uniform vec3 color;
+uniform float opacity;
+
+void main() {
+
+    if ( length( gl_PointCoord - vec2( 0.5, 0.5 ) ) > 0.475 ) discard;
+
+    gl_FragColor = vec4( color, opacity );
+
+}
+`;
+
+
+let logoFile = "./js/season.svg";
+
+
+let logoSVGObject;
+let loader = new SVGLoader();
+
+let logoMesh;
+let originalLogoVertecies;
+
+let frameCount = 0;
+
+let particleLimit = 2500;
+
+let paused = false;
+let pauseCount = 0;
+let pauseLength = 100;
+
+let randomVectors = [];
+
+let canvasArea = document.getElementById("canvas-area");
+
+let scene;
+let camera;
+let renderer;
+
+let ctx;
+
+let playback = true;
+
+document.getElementById("playback").onclick = function() {
+    playback = !playback;
+};
+
+let speed = document.getElementById("speed").value * 0.000005;
+document.getElementById("speed").oninput = function() {
+    speed = document.getElementById("speed").value * 0.000005;
+};
+
+let hDist = document.getElementById("hdist").value;
+document.getElementById("hdist").oninput = function() {
+    hDist = document.getElementById("hdist").value;
+    createRandomVectorArray();
+};
+
+let vDist = document.getElementById("vdist").value;
+document.getElementById("vdist").oninput = function() {
+    vDist = document.getElementById("vdist").value;
+    createRandomVectorArray();
+};
+
+let dsize = document.getElementById("dsize").value;
+document.getElementById("dsize").oninput = function() {
+    dsize = document.getElementById("dsize").value;
+};
+
+let twDist = document.getElementById("twdist").value*0.0000001;
+document.getElementById("twdist").oninput = function() {
+    twDist = document.getElementById("twdist").value*0.0000001;
+    createRandomVectorArray();
+};
+
+window.onload = initScene;
+
+function initScene() {
+
+    scene = new THREE.Scene();
+    camera;
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    camera = new THREE.PerspectiveCamera(30, canvasArea.clientWidth / canvasArea.clientHeight, 0.1, 10000);
+    camera.position.z = -955;
+    camera.lookAt(0,0,0);
+
+    renderer.setSize(canvasArea.clientWidth, canvasArea.clientHeight);
+    //renderer.setClearColor( 0xaffa00, 1 );
+    //renderer.clearColor();
+    canvasArea.appendChild(renderer.domElement);
+
+    loader.load(logoFile, addSVGtoScene,
+        // called when loading is in progresses
+        function ( xhr ) {
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        },
+        // called when loading has errors
+        function ( error ) {
+            console.log( 'An error happened' );
+        }
+    );
+
+}
+
+function addSVGtoScene( data ){
+
+    let paths = data.paths;
+    logoSVGObject = new THREE.Group();
+    let averageCenter = new THREE.Vector3();
+    for ( let i = 0; i < paths.length; i ++ ) {
+
+        let path = paths[ i ];
+
+        let material = new THREE.MeshBasicMaterial( {
+            transparent: true,
+            color: 0xFFFFFF,
+            side: THREE.DoubleSide,
+            depthWrite: false
+        } );
+
+        let shapes = path.toShapes( true );
+
+        for ( let j = 0; j < shapes.length; j ++ ) {
+
+            let shape = shapes[ j ];
+            let geometry = new THREE.ShapeBufferGeometry( shape );
+            let letterCenter = new THREE.Vector3();
+            geometry.computeBoundingBox();
+            geometry.boundingBox.getCenter(letterCenter);
+            averageCenter.x += letterCenter.x;
+            averageCenter.y += letterCenter.y;
+            
+            let mesh = new THREE.Mesh( geometry, material );
+            logoSVGObject.add( mesh );
+        }
+        averageCenter.x = averageCenter.x / (shapes.length);
+        averageCenter.y = averageCenter.y / (shapes.length);
+    }
+    let transform = new THREE.Matrix4();
+
+    if(canvasArea.clientHeight > canvasArea.clientWidth){
+        transform.makeScale(0.5, 0.5, 0.5)
+    } else {
+        transform.makeScale(1, 1, 1)
+    }
+    transform.multiply(new THREE.Matrix4().makeRotationZ(Math.PI));
+    transform.multiply(new THREE.Matrix4().makeTranslation(-averageCenter.x, -averageCenter.y, 0));
+    //
+    logoSVGObject.applyMatrix4(transform);
+
+
+    scene.add( logoSVGObject );
+    renderer.render(scene, camera);
+
+    let dotGeometry = new THREE.BufferGeometry();
+    let dotMaterial = new THREE.ShaderMaterial({
+        uniforms: {
+            color: {
+                value: new THREE.Color(0xFFFFFF)
+            },
+            size: {
+                value: dsize
+            },
+            opacity: {
+                value:   1
+            }
+        },
+        transparent: true,
+        vertexShader: vshader,
+        fragmentShader: fshader,
+    });
+
+    createLogoInParticles();
+    createRandomVectorArray();
+
+    dotGeometry.setAttribute( 'position', new THREE.BufferAttribute( originalLogoVertecies.slice(), 3 ) );
+    logoMesh = new THREE.Points(dotGeometry, dotMaterial);
+
+    scene.add(logoMesh);
+
+
+    animate();
+}
+
+function updatePos(mesh){
+    for(let i = 0; i < mesh.geometry.attributes.position.array.length; i++){
+        mesh.geometry.attributes.position.array[i] = originalLogoVertecies[i] + (randomVectors[i]*animationPercentage(frameCount));
+        mesh.material.uniforms.size.value = dsize;
+    }
+    mesh.geometry.attributes.position.needsUpdate = true;
+}
+
+function canvasScaleFactor(){
+    return Math.pow(canvasArea.clientWidth, 2) + Math.pow(canvasArea.clientHeight, 2);
+}
+
+function createRandomVectorArray() {
+    
+    for(let i = 0; i < originalLogoVertecies.length; i++){
+        let distFromCenter = 1-(
+                                Math.pow(originalLogoVertecies[(i*3)],   2) + 
+                                Math.pow(originalLogoVertecies[(i*3)+1], 2) +
+                                Math.pow(originalLogoVertecies[(i*3)+2], 2)
+                              )*twDist;
+
+        randomVectors[(i*3)] = (hDist-Math.random()*hDist*2) *distFromCenter;
+        randomVectors[(i*3)+1] = (vDist-Math.random()*vDist*2) *distFromCenter;
+        randomVectors[(i*3)+2] = (-800 + (Math.random()-0.5)*350);
+    }
+}
+
+function createLogoInParticles() {
+    let pointArray = [];
+    let gl = renderer.getContext();
+    let pixels = new Uint8Array(gl.drawingBufferWidth * gl.drawingBufferHeight * 4);
+    gl.readPixels(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+
+    let screenHeightAtOrigin = 2 * Math.tan((camera.fov * Math.PI / 180)/2) * Math.abs(camera.position.z);
+    let screenUnitDifference = screenHeightAtOrigin / gl.drawingBufferHeight;
+
+    for (let i = 0; i < pixels.length/4; i++) {
+            //read the pixel
+            let brightness = (pixels[i*4] + pixels[i*4 + 1] + pixels[i*4 + 2]) / 3;
+
+            if (brightness > 128) {
+                if(i%(Math.ceil((pixels.length*0.001) / particleLimit)) == 0){
+                    pointArray.push((Math.random()-0.5)+((gl.drawingBufferWidth / 2) - (i % gl.drawingBufferWidth))*screenUnitDifference);
+                    pointArray.push((Math.random()-0.5)+((gl.drawingBufferHeight / 2) - (i / gl.drawingBufferWidth))*-1*screenUnitDifference);
+                    pointArray.push(0);
+                }
+            };
+    }
+    
+    originalLogoVertecies = new Float32Array(pointArray.length);
+    for(let i = 0; i < pointArray.length; i++){
+            originalLogoVertecies[i] = pointArray[i];
+    }
+}
+
+
+function onWindowResize() {
+    camera.aspect = canvasArea.clientWidth / canvasArea.clientHeight;
+
+    camera.updateProjectionMatrix();
+    renderer.setSize(canvasArea.clientWidth, canvasArea.clientHeight);
+
+}
+
+function sigmoid(t, r) {
+    return 1 / (1 + Math.pow(Math.E, -(r + ((r * -2) * t))));
+}
+
+function pingpong(value, range1, range2){
+    let diff = range2 - range1;
+    value = value % (2*diff);
+    if(value < diff){
+        return range1+value;
+    } else {
+        return range1+(diff-(value-diff));
+    }
+}
+
+function animationPercentage(time) {
+    return sigmoid(pingpong(time, 0, 1), -12);
+}
+
+// Take the value inside of a range and returns as a percentage 
+function percentageBetweenValues(input, start, end){
+    let difference = end - start;
+    let distanceIntoRange = input - start;
+    return distanceIntoRange / difference; 
+}
+
+function animate() {
+    // camera.position.z = 0;
+    // camera.position.x = 900;
+    // camera.lookAt(0,0,0);
+    requestAnimationFrame(animate);
+    if (playback) {
+        updatePos(logoMesh);
+
+        if(!paused && animationPercentage(frameCount) < animationPercentage(speed)){
+            paused = true;
+        }
+        if(paused && pauseCount >= pauseLength){
+            pauseCount = 0;
+            frameCount = speed;
+            paused = false;
+        }
+        if(!paused){
+            frameCount+=speed;
+        } else {
+            pauseCount++;
+        }
+        logoMesh.material.uniforms.opacity.value = percentageBetweenValues(animationPercentage(frameCount), 0.0, 0.0005);
+        logoSVGObject.children[0].material.opacity =  1.1-percentageBetweenValues(animationPercentage(frameCount), 0.0, 0.0005);
+        
+        renderer.render(scene, camera);
+    }
+}
+
+
+window.onresize = onWindowResize;
